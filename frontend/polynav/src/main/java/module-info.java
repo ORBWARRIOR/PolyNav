55502370831a53249870fdaa3fedf0f2a115da3e
ORBWARRIOR <ymuhamm2@bradford.ac.uk>
2026-02-01 21:13:33 +0000

    Reconstruct project with maven

--- frontend/polynav/src/main/java/module-info.java
+++ frontend/polynav/src/main/java/module-info.java
@@ -1,4 +1,17 @@
 module io.github.orbwarrior {
-    requires javafx.controls;
+    requires transitive javafx.controls;
+    requires javafx.fxml;
+    requires com.google.protobuf;
+    requires io.grpc;
+    requires io.grpc.stub;
+    requires io.grpc.protobuf;
+    requires java.annotation;
+    requires com.google.common;
+    requires com.google.gson;
+
+    opens io.github.orbwarrior to javafx.fxml, com.google.gson;
+    opens fyp.generated to com.google.protobuf;
+    
     exports io.github.orbwarrior;
+    exports fyp.generated;
 }
